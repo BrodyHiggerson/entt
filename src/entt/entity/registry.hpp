9febb71368a7e4601b00971f7214d8d2771fa72c
Brody Higgerson <7245990+BrodyHiggerson@users.noreply.github.com>
2023-10-27 17:47:50 +1100

    Added context guard to new branch/version

--- src/entt/entity/registry.hpp
+++ src/entt/entity/registry.hpp
@@ -168,6 +168,7 @@ public:
 
     template<typename Type, typename... Args>
     Type &emplace_as(const id_type id, Args &&...args) {
+        LOCK_ENTT_REGISTRY_CONTEXT_GUARD();
         return any_cast<Type &>(ctx.try_emplace(id, std::in_place_type<Type>, std::forward<Args>(args)...).first->second);
     }
 
@@ -178,6 +179,7 @@ public:
 
     template<typename Type>
     Type &insert_or_assign(const id_type id, Type &&value) {
+        LOCK_ENTT_REGISTRY_CONTEXT_GUARD();
         return any_cast<std::remove_cv_t<std::remove_reference_t<Type>> &>(ctx.insert_or_assign(id, std::forward<Type>(value)).first->second);
     }
 
@@ -188,40 +190,47 @@ public:
 
     template<typename Type>
     bool erase(const id_type id = type_id<Type>().hash()) {
+        LOCK_ENTT_REGISTRY_CONTEXT_GUARD();
         const auto it = ctx.find(id);
         return it != ctx.end() && it->second.type() == type_id<Type>() ? (ctx.erase(it), true) : false;
     }
 
     template<typename Type>
     [[nodiscard]] const Type &get(const id_type id = type_id<Type>().hash()) const {
+        LOCK_ENTT_REGISTRY_CONTEXT_GUARD();
         return any_cast<const Type &>(ctx.at(id));
     }
 
     template<typename Type>
     [[nodiscard]] Type &get(const id_type id = type_id<Type>().hash()) {
+        LOCK_ENTT_REGISTRY_CONTEXT_GUARD();
         return any_cast<Type &>(ctx.at(id));
     }
 
     template<typename Type>
     [[nodiscard]] const Type *find(const id_type id = type_id<Type>().hash()) const {
+        LOCK_ENTT_REGISTRY_CONTEXT_GUARD();
         const auto it = ctx.find(id);
         return it != ctx.cend() ? any_cast<const Type>(&it->second) : nullptr;
     }
 
     template<typename Type>
     [[nodiscard]] Type *find(const id_type id = type_id<Type>().hash()) {
+        LOCK_ENTT_REGISTRY_CONTEXT_GUARD();
         const auto it = ctx.find(id);
         return it != ctx.end() ? any_cast<Type>(&it->second) : nullptr;
     }
 
     template<typename Type>
     [[nodiscard]] bool contains(const id_type id = type_id<Type>().hash()) const {
+        LOCK_ENTT_REGISTRY_CONTEXT_GUARD();
         const auto it = ctx.find(id);
         return it != ctx.cend() && it->second.type() == type_id<Type>();
     }
 
 private:
     dense_map<id_type, basic_any<0u>, identity, std::equal_to<id_type>, allocator_type> ctx;
+    DEFINE_ENTT_REGISTRY_CONTEXT_GUARD();
 };
 
 } // namespace internal
